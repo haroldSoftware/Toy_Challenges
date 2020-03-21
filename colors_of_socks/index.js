0eb9a1ec0a99f777726e96a1675fc5ec6c2ff8db
Ulrich <ulrich@pop-os.localdomain>
2020-03-20 23:55:47 -0700

    pairs of socks per color

--- colors_of_socks/index.js
+++ colors_of_socks/index.js
@@ -1,4 +1,6 @@
 //==============================================================================
+//-------------Function returns number of pairs of socks per color--------------
+//==============================================================================
 
 function colors_of_socks(inpAr) {
 
@@ -7,7 +9,9 @@ function colors_of_socks(inpAr) {
     return -1;
   }
 
-  let shadow = new Array(inpAr.length).fill(0);
+  let max = Math.max(...inpAr) + 1;
+
+  let shadow = new Array(max).fill(0);
 
 //------------------------------Fill The Shadow---------------------------------
   for (let i = 0; i < inpAr.length; i++) {
@@ -17,22 +21,25 @@ function colors_of_socks(inpAr) {
     }
     shadow[inpAr[i]]++;
   }
-  console.log("shadow", shadow)
 //------------------------------------------------------------------------------
 
   let pairs = [];
+  let pairCount = 0;
 
   for (let k = 0; k < shadow.length; k++) {
     let position;
     let mod;
     if ( (shadow[k] != undefined) ) {
-      position = k; 
+      position = k;
       mod = shadow[k] / 2;
-      pairs.push({color: position, numPairs: mod})
+      pairs.push({color: position, numPairs: mod});
+      let temp = Math.floor(mod % 2);
+      pairCount += temp;
     }
   }
 
-  return pairs;
+  console.log("entire aray: ", pairs);
+  return pairCount;
 
 }
 
@@ -41,9 +48,16 @@ function colors_of_socks(inpAr) {
 //==============================================================================
 let n = 7;
 let ar = [1,2,1,2,1,3,2];
+let ar2 = [5,7,3,5,3,6,7,8,9,3];
+let ar3 = [5,7,3,5,3,6,7,8,9,3];
 
-let testArr = [9,10,20,20,10,10,30,50,10, 20];
-
+console.log("=============");
 console.log(colors_of_socks(ar));
+console.log("=============");
+console.log(colors_of_socks(ar2));
+console.log("=============");
+console.log(colors_of_socks(ar3));
+console.log("=============");
+
 
 //==============================================================================
